3917be57cc9d558334b86e18e98fe068cff78412
yitznewton <yitznewton@hotmail.com>
2015-05-28 23:22:54 -0400

    Refactor appending a movie to the movie list
    Here, I've pulled out the code for appending a movie into its own
    function. I also gave the <ul> an ID, since we'll have another <ul> for
    retrieved favorites.

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,9 @@
 (function(){
+  var appendMovie = function(ul, result) {
+    var node = document.createElement('li');
+    node.innerText = result.Title;
+    ul.appendChild(node);
+  };
 
   document.querySelector('form').addEventListener('submit', function(event){
     event.preventDefault();
@@ -6,11 +11,10 @@
     var xhr = new XMLHttpRequest();
     xhr.open('get', 'http://omdbapi.com/?s=' + encodeURIComponent(input), true);
     xhr.addEventListener('load', function(response){
-      var res =  JSON.parse(this.response).Search;
-      for(var i = 0; i < res.length; i++){
-        var node = document.createElement('li');
-        node.innerText = res[i].Title;
-        document.querySelector('ul').appendChild(node);
+      var results = JSON.parse(this.response).Search;
+      var ul = document.querySelector('#movie-list')
+      for(var i = 0; i < results.length; i++){
+        appendMovie(ul, results[i]);
       }
   });
     xhr.send();
