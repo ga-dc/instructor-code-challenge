e76c8bdc17c31fec63408d791e5f9aa216d46599
yitznewton <yitznewton@hotmail.com>
2015-05-29 00:49:01 -0400

    Add HTML favorites view
    Here, I've added a browser HTML view of favorites, in addition to the
    JSON API endpoint. I extracted the retrieval of movies from the data
    file into a function, and used `provides` conditions to select which
    route to used based on the type requested (JSON or HTML).

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'bundler/setup'
 require 'sinatra'
 require 'json'  # <== missing this `require`
 
+def all_movies
+  JSON.parse(File.read('data.json')) rescue []
+end
+
 # get '/'  <== missing `do`
 get '/' do
   #File.read('index.html')  <== missing path to file
@@ -9,7 +13,12 @@ get '/' do
 end
 
 # get 'favorites' do  <== missing initial slash
-get '/favorites' do
+get '/favorites', provides: :html do
+  @movies = all_movies.sort { |a, b| a['name'] <=> b['name'] }
+  erb :favorites
+end
+
+get '/favorites', provides: :json do
   response.header['Content-Type'] = 'application/json'
   File.read('data.json')
 end
@@ -17,14 +26,15 @@ end
 # get '/favorites' do  <== this should be a post, not a get
 post '/favorites' do
   # file = JSON.parse(File.read('data.json'))  <== no handling for empty file
-  file = JSON.parse(File.read('data.json')) rescue []
   400 unless params[:name] && params[:oid]
-  movie = { name: params[:name], oid: params[:oid] }
-  file << movie
-  File.write('data.json',JSON.pretty_generate(file))
-  movie.to_json
+  new_movie = { name: params[:name], oid: params[:oid] }
+  movies = all_movies
+  movies << new_movie
+  File.write('data.json',JSON.pretty_generate(movies))
+  new_movie.to_json
 end
 
 error 400 do
   'Invalid Request'
 end
+
